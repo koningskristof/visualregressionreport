f7398d4fd0c08a54ed84c63012a911b05c593487
Kristof Konings <kristof.konings@dsquare.be>
2016-03-22 14:34:13 +0100

    Clear config example.

--- example_visualregressionreport.config.js
+++ example_visualregressionreport.config.js
@@ -1,5 +1,3 @@
 exports.config = {
-    screenshotFolder: 'test/e2e/screenshots/',
-    screenshotBaseFolder: 'test/e2e/screenshots/base/',
-    screenshotDiffFolder: 'test/e2e/screenshots/diff/'
+    screenshotFolder: 'test/e2e/screenshots/'
 };
